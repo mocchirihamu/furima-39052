6cd9b2c43f2529eb8f2265b22a94478b4d7892aa
mocchirihamu <tech.0813.1114@gmail.com>
2023-03-12 13:57:33 +0900

    Rubocopの実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,8 +34,8 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
       it 'passwordが129文字以上では登録できない' do
-        @user.password =  Faker::Internet.password(min_length: 129)
-        @user.password_confirmation =  @user.password
+        @user.password = Faker::Internet.password(min_length: 129)
+        @user.password_confirmation = @user.password
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
       end
@@ -81,7 +81,7 @@ RSpec.describe User, type: :model do
         @user.birthday = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
-      end      
+      end
     end
   end
 end
