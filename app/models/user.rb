111477ce5377b4f43abf47ad0c25ae11cd5c0476
mocchirihamu <tech.0813.1114@gmail.com>
2023-03-12 12:34:40 +0900

    新規会員登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }, presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }, presence: true
+  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
   validates :birthday, presence: true
 
 
