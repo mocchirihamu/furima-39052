bf2c7f2e502944a1debc6f02cf3130a26fd83898
mocchirihamu <tech.0813.1114@gmail.com>
2023-03-13 20:06:41 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,14 @@
 class ItemsController < ApplicationController
-end
+  before_action :authenticate_user!, only: [:new]
 
-private
+  def new
+  @item = Item.new
+  end
 
-def item_params
-  params.require(:item).permit(:image).merge(user_id: current_user.id)
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:detail,:category_id,:condition_id,:delivery_charge_id,:prefecture_id,:shopping_date_id,:price,:image).merge(user_id: current_user.id)
+  end
+
 end
