ccde442bb1ab4c2959707281ac64d60645a116e3
mocchirihamu <tech.0813.1114@gmail.com>
2023-03-13 18:32:28 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :shopping_date
+
+
   has_one_attached :image
 end
