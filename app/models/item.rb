775cd3860de2f846e18ac909ac3e993c54639756
mocchirihamu <tech.0813.1114@gmail.com>
2023-03-13 19:54:59 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,15 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shopping_date
 
-
+  belongs_to :user
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :detail, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shopping_date_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, presence: true
 end
