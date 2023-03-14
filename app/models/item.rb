41850224ba88c99ece99c288ce49accbe2e6868d
mocchirihamu <tech.0813.1114@gmail.com>
2023-03-14 18:42:31 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,13 +11,13 @@ class Item < ApplicationRecord
 
   with_options presence: true do
    validates :image
-   validates :name
-   validates :detail
-   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
-   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
-   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
-   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
-   validates :shopping_date_id, numericality: { other_than: 1 , message: "can't be blank"} 
+   validates :name, length: { maximum: 140 }
+   validates :detail, length: { maximum: 1000 }
+   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+   validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+   validates :shopping_date_id, numericality: { other_than: 1, message: "can't be blank" }
    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}, format: { with: /\A[0-9]+\z/ }
   end
 end
