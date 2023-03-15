9a224087859ef7def77052a3439a41e242241e74
mocchirihamu <tech.0813.1114@gmail.com>
2023-03-15 18:31:51 +0900

    編集画面への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
   private
 
   def item_params
