ed05ce95c51d9bfc559b1aad99c05ca06e53f0b5
mocchirihamu <tech.0813.1114@gmail.com>
2023-03-23 17:07:18 +0900

    テストコードの追加

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -8,5 +8,6 @@ FactoryBot.define do
     telephone              { '08012121212' }
     user_id                   { '1' }
     item_id                   { '1' }
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -61,5 +61,10 @@ RSpec.describe OrderAddress, type: :model do
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Telephone is invalid")
     end
+    it "トークンが空では購入できない" do
+      @order_address.token = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Token can't be blank")
+    end
   end
 end
